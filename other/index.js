296b1b0b6adad07d62a14e0ad5d13b562f7fd59f
LCL121 <1391436522@qq.com>
2020-07-26 16:11:03 +0800

    Add other

---
+++ other/index.js
@@ -0,0 +1,5 @@
+if (document.documentElement.offsetWidth <= 500) {
+  window.location.href = 'http://39.97.209.106/fresh_sxu/mobile/#/'
+} else {
+  window.location.href = 'http://39.97.209.106/fresh_sxu/pc/#/'
+}
